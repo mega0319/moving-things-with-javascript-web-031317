01043b272ca524f53046561c936df6082b186a87
Naz Khan <nazmuskhan@yahoo.com>
2017-04-30 15:28:04 -0400

    finished

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const app = "I don't do much."
+
+function moveDodgerRight() {
+    var rightNumbers =
+        dodger.style.right.replace('px', '')
+    var right = parseInt(rightNumbers, 10)
+    if(right > 0) {
+        dodger.style.right = `${right - 1}px`
+    }
+}
+
+document.addEventListener('keydown', function(key){
+    return console.log(key)
+})
+
+document.addEventListener("keydown", function(key){
+    if (key.which === 39) {
+        moveDodgerRight()
+    }
+})
